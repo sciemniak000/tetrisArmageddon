8a5df55cefd103a8de26c2e74948f05ff2caa8a5
Przemek <sciemniak000@gmail.com>
2019-08-28 11:59:32 +0200

    Elements positioned on the screen
    # To be improved yet

--- scripts/application.js
+++ scripts/application.js
@@ -1,3 +1,103 @@
-class ApplicationContainer{
+export class ApplicationContainer{
+    constructor(){
+        let div = document.createElement("div");
+        div.id = "container";
+        div.style.display = "block";
+        div.style.marginLeft = "auto";
+        div.style.marginRight = "auto";
+        div.style.width = "10px";
+        div.style.height = "10px";
+        document.body.appendChild(div);
+        this.div2 = document.createElement("div");
+        this.div2.id = "positioner";
+        this.div2.style.display = "block";
+        this.div2.style.marginRight = "auto";
+        this.div2.style.marginLeft = "auto";
+        this.div2.style.position = "relative";
+        div.appendChild(this.div2);
+    }
 
+    createGameWindow(){
+        let canv = document.createElement("canvas");
+        canv.id = "game_area";
+        canv.width = 279;
+        canv.height = 559;
+        canv.style.border = "1px solid #d3d3d3";
+        canv.style.backgroundColor = "#000000";
+
+        canv.style.position = "absolute";
+        canv.style.left = "-135px";
+
+        // canv.style.marginRight = "auto";
+        // canv.style.marginLeft = "auto";
+        // canv.style.display = "block";
+
+        this.div2.appendChild(canv);
+        this.canv_game = canv;
+    }
+
+    createBlackWindowCanvas(id){
+        let canv = document.createElement("canvas");
+        canv.id = id;
+        canv.width = 50;
+        canv.height = 50;
+        canv.style.border = "1px solid #d3d3d3";
+        canv.style.backgroundColor = "#000000";
+        return canv;
+    }
+
+    createTimerCanvas(){
+        this.timer_canvas = document.createElement("canvas");
+        this.timer_canvas.id = "timer_area";
+        this.timer_canvas.width = 100;
+        this.timer_canvas.height = 100;
+        this.timer_canvas.style.border = "1px solid #d3d3d3";
+        this.timer_canvas.style.backgroundColor = "#000000";
+        // this.timer_canvas.style.backgroundColor = "maroon";
+        this.timer_canvas.style.position = "absolute";
+        this.timer_canvas.style.bottom = "-559px";
+        this.timer_canvas.style.left = "-250px";
+        this.div2.appendChild(this.timer_canvas);
+    }
+
+    createSwitchWindow(){
+        let sw = this.createBlackWindowCanvas("switch");
+        sw.style.position = "absolute";
+        sw.style.left = "-200px";
+        sw.style.top = "10px";
+        this.div2.appendChild(sw);
+    }
+
+    createFifoWindows(){
+        let w1 = this.createBlackWindowCanvas("fifo1");
+        let w2 = this.createBlackWindowCanvas("fifo2");
+        let w3 = this.createBlackWindowCanvas("fifo3");
+        let w4 = this.createBlackWindowCanvas("fifo4");
+        let w5 = this.createBlackWindowCanvas("fifo5");
+
+        w1.style.position = "absolute";
+        w1.style.right = "-200px";
+        w1.style.top = "10px";
+        this.div2.appendChild(w1);
+
+        w2.style.position = "absolute";
+        w2.style.right = "-200px";
+        w2.style.top = "70px";
+        this.div2.appendChild(w2);
+
+        w3.style.position = "absolute";
+        w3.style.right = "-200px";
+        w3.style.top = "130px";
+        this.div2.appendChild(w3);
+
+        w4.style.position = "absolute";
+        w4.style.right = "-200px";
+        w4.style.top = "190px";
+        this.div2.appendChild(w4);
+
+        w5.style.position = "absolute";
+        w5.style.right = "-200px";
+        w5.style.top = "250px";
+        this.div2.appendChild(w5);
+    }
 }
---
+++ scripts/launch.js
@@ -0,0 +1,9 @@
+import {ApplicationContainer} from "./application.js";
+
+let a = new ApplicationContainer();
+
+a.createGameWindow();
+a.createSwitchWindow();
+a.createSwitchWindow();
+a.createTimerCanvas();
+a.createFifoWindows();
